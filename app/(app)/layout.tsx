5badf8d48b7664bf9729fcf6b4c102af44c6d4e6
Trophy198 <spsp8426@naver.com>
2024-06-29 21:05:00 +0900

    feat: 레이아웃 oauthprovider 연동

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,20 +1,22 @@
+import { ReactNode } from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import Provider from '../components/Theme/Provider';
-import './globals.css';
 import Header from '../components/common/Header';
+import './globals.css';
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html suppressHydrationWarning lang="ko">
       <body>
-        <Provider>
-          <Header />
-          {children}
-        </Provider>
+        <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_ID || ''}>
+          <Provider>
+            <Header />
+            {children}
+          </Provider>
+        </GoogleOAuthProvider>
       </body>
     </html>
   );
-}
+};
+
+export default RootLayout;

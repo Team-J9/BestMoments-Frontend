2fadd1f323413111c66f1553e626aa542c554577
Trophy198 <spsp8426@naver.com>
2024-07-02 23:23:04 +0900

    design: GNB 헤더 컴포넌트 추가

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,10 +1,24 @@
-import Switcher from '../Theme/Switcher';
+import Link from 'next/link';
+import ThemeSwitcher from '../Theme/Switcher';
+import Image from 'next/image';
+import Button from './Button';
 
 const Header = () => {
   return (
-    <div>
-      <Switcher />
-    </div>
+    <header className="flex border-[#6E6E82] border-b-2 p-5 justify-between">
+      <Link className="flex justify-center items-center" href="/">
+        <Image src="/logo.png" width={30} height={30} alt="logo" priority className="w-10 h-10 sm:w-8 sm:h-8" />
+        <h1 className="hidden sm:block text-xl sm:text-2xl ml-2">
+          BEST<span className="text-blue-400">MOMENTS</span>
+        </h1>
+      </Link>
+      <nav className="flex justify-center items-center gap-5">
+        <ThemeSwitcher />
+        <Button as="link" href="/login">
+          로그인
+        </Button>
+      </nav>
+    </header>
   );
 };
 

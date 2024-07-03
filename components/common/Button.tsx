46982007ebdfd9e5302c22847c8d6cd7f8c416a1
Trophy198 <spsp8426@naver.com>
2024-07-03 21:50:26 +0900

    refactor: 버튼 컴포넌트 인터페이스 선언 방식 변경

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -10,7 +10,7 @@ type ButtonProps = {
   className?: string;
 };
 
-const Button: React.FC<ButtonProps> = ({
+const Button = ({
   as = 'button',
   href,
   children,
@@ -18,7 +18,7 @@ const Button: React.FC<ButtonProps> = ({
   disabled = false,
   className = '',
   ...props
-}) => {
+}: ButtonProps) => {
   const baseClasses =
     'flex items-center disabled:opacity-50 disabled:hover:opacity-50 justify-center ring-none rounded-lg shadow-lg font-semibold py-2 px-4 font-dm focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-400 border-blue-700 disabled:border-0 disabled:bg-blue-500 disabled:text-white ring-white text-white border-b-4 transition duration-300 ease-in-out hover:bg-blue-500 hover:border-blue-800 hover:shadow-none active:shadow-inner active:bg-blue-800 active:text-gray-300 focus-visible:outline-blue-500 text-sm sm:text-base dark:hover:bg-blue-800 dark:bg-blue-700 dark:border-blue-700 dark:border-b-blue-900';
 
